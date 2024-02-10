975b492efabb8a2fb3a264e70e35f01730caa8d3
Rama Adhitya Setiadi <ramaadhityasetiadi002@gmail.com>
2024-02-10 23:26:56 +0700

    bugfix/input-login

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
 import Guest from '../../layouts/Guest'
 import ButtonOutline from '../../../components/ButtonOutline/ButtonOutline'
+import { FloatingLabel } from 'flowbite-react'
 
 export default function Login() {
     const navigate = useNavigate()
@@ -16,22 +17,22 @@ export default function Login() {
             <div>
                 <form className='pt-10'>
                     <div className='space-y-3 py-2'>
-                        <label htmlFor="username" className='input-label'>Username</label>
-                        <input type="text" id='username' name='username' className='w-full input' placeholder='username' required />
+                        <FloatingLabel variant="standard" label="Username" className='text-white peer-focus:text-white focus:border-white' />
                     </div>
-                    <div className='space-y-3 py-2'>
-                        <div className="flex items-center justify-between">
-                            <label htmlFor="password" className='input-label'>Password</label>
-                            <NavLink to={'/forgot-password'} className='text-white hover:underline font-semibold'>Lupa Password?</NavLink>
+                    <div>
+                        <div className="flex items-center justify-end">
+                            <NavLink to={'/forgot-password'} className='text-white hover:underline font-semibold text-sm'>Lupa Password?</NavLink>
                         </div>
-                        <input type="password" id='password' name='password' className='w-full input' placeholder='******' required />
+                        <div className='-mt-2'>
+                            <FloatingLabel variant="standard" label="Password" type='password' className='text-white peer-focus:text-white focus:border-white' />
+                        </div>
                     </div>
 
-                    <div>
+                    <div className='mt-8'>
                         <p className='text-white'>Belum punya akun? <NavLink to={'/register'} className='underline font-semibold'>Daftar Sekarang</NavLink></p>
                     </div>
 
-                    <div className='pt-10'>
+                    <div className='pt-7'>
                         <ButtonOutline onClick={() => navigate('/dashboard')}>Login</ButtonOutline>
                     </div>
                 </form>
